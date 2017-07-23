80840e99d427b074a31c4e371849f0f12c02b77b
wcarranzab <waltericardo10@gmail.com>
2017-07-23 00:34:19 -0500

    wcarranza-23072017-mejora del service login
    Signed-off-by: wcarranzab <waltericardo10@gmail.com>

--- src/RestApi/LabOnTime/Api.LabOnTime.Entities/User.cs
+++ src/RestApi/LabOnTime/Api.LabOnTime.Entities/User.cs
@@ -4,12 +4,20 @@
     {
         public int id { get; set; }
         public int roles_id { get; set; }
+        public string description { get; set; }
         public int persons_id { get; set; }
         public string name { get; set; }
         public string password { get; set; }
-        public bool active { get; set; }
+        public string names { get; set; }
+        public string lastnames { get; set; }
         public string address { get; set; }
-        public string email { get; set; }
         public string phone { get; set; }
+        public string email { get; set; }
+        public string documentnumber { get; set; }
+        public string ruc { get; set; }
+        public string type { get; set; }
+        public string bussinessname { get; set; }
+        public string doctornumber { get; set; }
+        public string speciality { get; set; }
     }
 }
--- src/RestApi/LabOnTime/Api.LabOnTime/Controllers/LoginController.cs
+++ src/RestApi/LabOnTime/Api.LabOnTime/Controllers/LoginController.cs
@@ -17,11 +17,22 @@ namespace Api.LabOnTime.Controllers
             var dt = blLogin.ValidateUser(User, Password);
             LoginModel loginModel = new LoginModel();
             loginModel.user.id = int.Parse(dt.Rows[0]["id"].ToString());
-            loginModel.user.roles_id = int.Parse(dt.Rows[0]["roles_id"].ToString()); 
+            loginModel.user.roles_id = int.Parse(dt.Rows[0]["roles_id"].ToString());
+            loginModel.user.description = dt.Rows[0]["description"].ToString();
             loginModel.user.persons_id = int.Parse(dt.Rows[0]["persons_id"].ToString()); 
             loginModel.user.name = dt.Rows[0]["name"].ToString();
             loginModel.user.password = dt.Rows[0]["password"].ToString();
-            loginModel.user.active = Convert.ToBoolean(dt.Rows[0]["active"]);
+            loginModel.user.name = dt.Rows[0]["name"].ToString();
+            loginModel.user.lastnames = dt.Rows[0]["lastnames"].ToString();
+            loginModel.user.address = dt.Rows[0]["address"].ToString();
+            loginModel.user.phone = dt.Rows[0]["phone"].ToString();
+            loginModel.user.email = dt.Rows[0]["email"].ToString();
+            loginModel.user.documentnumber = dt.Rows[0]["documentnumber"].ToString();
+            loginModel.user.ruc = dt.Rows[0]["ruc"].ToString();
+            loginModel.user.type = dt.Rows[0]["type"].ToString();
+            loginModel.user.bussinessname = dt.Rows[0]["bussinessname"].ToString();
+            loginModel.user.doctornumber = dt.Rows[0]["doctornumber"].ToString();
+            loginModel.user.speciality = dt.Rows[0]["speciality"].ToString();
 
             if (dt == null)
             {
